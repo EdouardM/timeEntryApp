9b7b315fe68e4dac5a14f6df0affa952e0bad7f2
Edouard Moureaux <edouard.moureaux@gmail.com>
2016-11-18 12:04:35 +0100

    Creation of types to work with Database

--- src/TimeEntry/Database.fs
+++ src/TimeEntry/Database.fs
@@ -1,6 +1,39 @@
 namespace TimeEntry
     open DomainTypes
     module DataBase =
+
+            type DBWorkOrder =
+                {
+                    WorkOrder : string
+                    ItemCode  : string
+                    Weight    : float
+                }
+
+            let toWorkOrderDB (wo: WorkOrderEntry) =
+                let (WorkOrder strWo) = wo.WorkOrder
+                let (ItemCode strItem) = wo.ItemCode
+                let (Weight w) = wo.Weight
+                { WorkOrder = strWo; ItemCode = strItem; Weight = w}
+
+            let fromWorkOrderDB (wo: DBWorkOrder) = ()
+                //Return WorkOrder Entry Result
+
+            type DBEventInfo =
+                {
+                    Event       : string
+                    Machine     : string
+                    Cause       : string
+                    Solution    : string
+                    Comments    : string
+                }
+
+            let toEventInfoDB (ev: EventEntry) =
+                ()
+
+
+            let fromEventInfoDB (wo: DBEventInfo) =
+                //Return WorkOrder Entry
+
             //DataBase model (pure)
             type DBTimeRecord =
                 {
@@ -8,12 +41,23 @@ namespace TimeEntry
                     ShopFloor   : ShopFloor
                     WorkCenter  : WorkCenter
                     TimeType    : TimeType
-                    Duration    : Duration
+                    DurationMn  : int
                     NbPeople    : NbPeople
+                    Allocation  : string // String workOrder or Event
+                    WorOrder    : DBWorkOrder option
+                    Event       : DBEventInfo option
                 }
 
+            let allocationToString =
+                function
+                    | WorkOrderEntry wo -> "WorkOrder"
+                    | EventEntry ev     -> "Event"
+
+
             ///Function to convert one Domain Record into records to insert into Database
-            let toDB  (time : TimeRecord) = 
+            let toTimeRecordDB  (time : TimeRecord) = 
+                let allocation = allocationToString time.Allocation
+
                 match time.TimeEntry with
                     //List of one record: Machine time
                     | MachineOnly duration -> 
@@ -22,8 +66,10 @@ namespace TimeEntry
                             ShopFloor   = time.ShopFloor 
                             WorkCenter  = time.WorkCenter 
                             TimeType    = MachineTime
-                            Duration    = duration
-                            NbPeople    = NbPeople 0. } ]
+                            DurationMn    = int duration.Duration.TotalMinutes
+                            NbPeople    = NbPeople 0.
+                            Allocation  = allocation
+                             } ]
                     //List of two records: Machine & Labour time
                     | MachineAndLabour (duration, nb) -> 
                         [ { 
@@ -31,15 +77,15 @@ namespace TimeEntry
                             ShopFloor   = time.ShopFloor 
                             WorkCenter  = time.WorkCenter
                             TimeType    = MachineTime 
-                            Duration    = duration
+                            DurationMn    = int duration.Duration.TotalMinutes
                             NbPeople    = NbPeople 0. } ;
 
                             { 
                             Site        = time.Site
                             ShopFloor   = time.ShopFloor 
                             WorkCenter  = time.WorkCenter
-                            TimeType    = MachineTime 
-                            Duration    = duration
+                            TimeType    = LabourTime 
+                            DurationMn    = int duration.Duration.TotalMinutes
                             NbPeople    = nb }]
                     //List of one record: Labour time
                     | LabourOnly (duration, nb) -> 
@@ -50,3 +96,5 @@ namespace TimeEntry
                             TimeType    = LabourTime
                             Duration    = duration 
                             NbPeople    = nb } ]
+            
+            let fromTimeRecordDB (time: DBTimeRecord) = ()
