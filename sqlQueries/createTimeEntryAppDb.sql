81af531fcdd6f14546b35126e65f71dcd6ceb604
Edouard Moureaux <edouard.moureaux@gmail.com>
2016-11-19 19:08:05 +0100

    Create first table

--- sqlQueries/createTimeEntryAppDb.sql
+++ sqlQueries/createTimeEntryAppDb.sql
@@ -1,2 +1,13 @@
+/*
+To connect to server, use the following command in cmd prompt:
+mysql -h 127.0.0.1 -u root -P 3306 
+
+*/
+--https://dev.mysql.com/doc/refman/5.5/en/creating-database.html
 CREATE DATABASE timeentryapp;
 SHOW DATABASES;
+
+--https://dev.mysql.com/doc/refman/5.5/en/creating-tables.html
+USE timeentryapp; 
+SHOW TABLES;
+
--- sqlQueries/createTimeRecord.sql
+++ sqlQueries/createTimeRecord.sql
@@ -1,7 +1,11 @@
+USE timeentryapp;
+
 CREATE TABLE TIMERECORD (
     Site VARCHAR(4),
     Shopfloor VARCHAR(4),
     TimeType INT, 
     DurationMn INT,
     NbPeople INT
-)
+);
+
+SHOW TABLES;
