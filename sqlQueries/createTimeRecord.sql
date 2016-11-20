6ff8f56fdfa1ad358328de5466ed6cdc76746aa3
Edouard Moureaux <edouard.moureaux@gmail.com>
2016-11-20 18:04:54 +0100

    Update create Table Script

--- sqlQueries/createTimeRecord.sql
+++ sqlQueries/createTimeRecord.sql
@@ -1,11 +1,22 @@
-USE timeentryapp;
+-- https://github.com/swlaschin/low-risk-ways-to-use-fsharp-at-work/blob/master/SqlInFsharp/CreateTables.sql
+USE timeentryapp
 
-CREATE TABLE TIMERECORD (
-    Site VARCHAR(4),
-    Shopfloor VARCHAR(4),
-    TimeType INT, 
-    DurationMn INT,
-    NbPeople INT
+/* =====================================================
+Create Time Record table  
+===================================================== */
+
+IF EXISTS (SELECT * FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_NAME = 'TimeRecord' AND TABLE_SCHEMA='dbo')
+   DROP TABLE dbo.TimeRecord
+GO
+
+CREATE TABLE TimeRecord (
+    TimeRecordId INT NOT NULL IDENTITY(1,1),
+    Site VARCHAR(4) NOT NULL,
+    Shopfloor VARCHAR(4) NOT NULL,
+    TimeType INT NOT NULL, 
+    DurationMn INT NOT NULL,
+    NbPeople INT NOT NULL
+
+    CONSTRAINT PK_TimeRecord PRIMARY KEY CLUSTERED (TimeRecordId)
 );
 
-SHOW TABLES;
