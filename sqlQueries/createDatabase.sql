b16186e90e858ea687af8bdc7835104930e2065d
Edouard Moureaux <edouard.moureaux@gmail.com>
2016-11-20 18:21:12 +0100

    Remove unued dll files nuget package only useful

--- sqlQueries/createDatabase.sql
+++ sqlQueries/createDatabase.sql
@@ -5,9 +5,9 @@ mysql -h 127.0.0.1 -u root -P 3306
 */
 --https://dev.mysql.com/doc/refman/5.5/en/creating-database.html
 CREATE DATABASE timeentryapp;
-SHOW DATABASES;
+--SHOW DATABASES;
 
 --https://dev.mysql.com/doc/refman/5.5/en/creating-tables.html
 USE timeentryapp; 
-SHOW TABLES;
+--SHOW TABLES;
 
